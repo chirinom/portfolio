26e5982bf788461a1e47ab77e8feabb2c4bb7fab
miguel chirino <mchirinot@gmail.com>
2020-11-01 18:04:53 +0100

    skills styling change

--- js/script.js
+++ js/script.js
@@ -3,15 +3,19 @@
 const htmlBar = document.querySelector('.bar-html')
 const cssBar = document.querySelector('.bar-css')
 const jsBar = document.querySelector('.bar-javascript')
-const rorBar = document.querySelector('.bar-ror')
 const bsBar = document.querySelector('.bar-bootstrap')
-const rubyBar = document.querySelector('.bar-ruby')
+const designBar = document.querySelector('.bar-design')
+const rorBar = document.querySelector('.bar-ror')
 const oopBar = document.querySelector('.bar-oop')
 const mvcBar = document.querySelector('.bar-mvc')
+const sqlBar = document.querySelector('.bar-sql')
+const arBar = document.querySelector('.bar-ar')
 const ggBar = document.querySelector('.bar-gg')
+const ghBar = document.querySelector('.bar-gh')
 const figmaBar = document.querySelector('.bar-figma')
 const herokuBar = document.querySelector('.bar-heroku')
-const saBar = document.querySelector('.bar-sa')
+const asBar = document.querySelector('.bar-as')
+
 
 let t1 = new TimelineLite();
 
@@ -19,14 +23,17 @@ t1.fromTo(htmlBar, .15, {width: `calc(0% - 6px)`}, {width: `calc(90% - 6px)`, ea
   .fromTo(cssBar, .15, {width: `calc(0% - 6px)`}, {width: `calc(85% - 6px)`, ease: Power4.easeOut})
   .fromTo(jsBar, .15, {width: `calc(0% - 6px)`}, {width: `calc(70% - 6px)`, ease: Power4.easeOut})
   .fromTo(bsBar, .15, {width: `calc(0% - 6px)`}, {width: `calc(95% - 6px)`, ease: Power4.easeOut})
-  .fromTo(rubyBar, .15, {width: `calc(0% - 6px)`}, {width: `calc(85% - 6px)`, ease: Power4.easeOut})
+  .fromTo(designBar, .15, {width: `calc(0% - 6px)`}, {width: `calc(80% - 6px)`, ease: Power4.easeOut})
   .fromTo(rorBar, .15, {width: `calc(0% - 6px)`}, {width: `calc(85% - 6px)`, ease: Power4.easeOut})
   .fromTo(oopBar, .15, {width: `calc(0% - 6px)`}, {width: `calc(75% - 6px)`, ease: Power4.easeOut})
   .fromTo(mvcBar, .15, {width: `calc(0% - 6px)`}, {width: `calc(90% - 6px)`, ease: Power4.easeOut})
+  .fromTo(sqlBar, .15, {width: `calc(0% - 6px)`}, {width: `calc(95% - 6px)`, ease: Power4.easeOut})
+  .fromTo(arBar, .15, {width: `calc(0% - 6px)`}, {width: `calc(85% - 6px)`, ease: Power4.easeOut})
   .fromTo(ggBar, .15, {width: `calc(0% - 6px)`}, {width: `calc(85% - 6px)`, ease: Power4.easeOut})
+  .fromTo(ghBar, .15, {width: `calc(0% - 6px)`}, {width: `calc(95% - 6px)`, ease: Power4.easeOut})
   .fromTo(figmaBar, .15, {width: `calc(0% - 6px)`}, {width: `calc(75% - 6px)`, ease: Power4.easeOut})
   .fromTo(herokuBar, .15, {width: `calc(0% - 6px)`}, {width: `calc(90% - 6px)`, ease: Power4.easeOut})
-  .fromTo(saBar, .15, {width: `calc(0% - 6px)`}, {width: `calc(80% - 6px)`, ease: Power4.easeOut})
+  .fromTo(asBar, .15, {width: `calc(0% - 6px)`}, {width: `calc(90% - 6px)`, ease: Power4.easeOut})
 
 const contoller = new ScrollMagic.Controller()
 const scene = new ScrollMagic.Scene({
@@ -35,6 +42,8 @@ const scene = new ScrollMagic.Scene({
 })
 .setTween(t1)
 .addTo(contoller)
+
+
 
 // ------------nav tabs-----------
 
